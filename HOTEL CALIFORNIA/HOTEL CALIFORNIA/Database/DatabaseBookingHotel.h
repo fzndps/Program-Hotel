a70156e5ed342b28f3883550a3f76f370c63090d
fzndps <149759891+fzndps@users.noreply.github.com>
2024-01-26 23:56:53 +0700

    Delete HOTEL CALIFORNIA/HOTEL CALIFORNIA directory

--- HOTEL CALIFORNIA/HOTEL CALIFORNIA/Database/DatabaseBookingHotel.h
+++
@@ -1,8 +0,0 @@
-#include <string>
-using namespace std;
-
-
-string jenisKamarKosong[100];
-string tanggalBooking[100];
-string tanggalCheckout[100];
-int nBooking;
--- HOTEL CALIFORNIA/HOTEL CALIFORNIA/Database/DatabaseHotel.h
+++
@@ -1,8 +0,0 @@
-#include <string>
-using namespace std;
-
-string namaHotel[100];
-string jenisKamar[100];
-int jumlahKamar[100];
-
-int nHotel;
